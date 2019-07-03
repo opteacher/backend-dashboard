865a42a787dcc501e97d8484be1fb7493fac5cb7
赵佳辰 <opteacher@hotmail.com>
2019-07-03 17:37:04 +0800

    切换d3.js处理图形化界面

--- frontend/src/panels/modelsPanel.vue
+++ frontend/src/panels/modelsPanel.vue
@@ -1,12 +1,11 @@
 <template>
     <div class="w-100 h-100">
         <tool-bar @add-model="addModel" @add-link="addLink" :models="models"/>
-        <div id="pnlModels" class="w-100 h-100"></div>
+        <svg id="pnlModels" class="w-100 h-100"></svg>
     </div>
 </template>
 
 <script>
-    import echarts from "echarts"
     import toolBar from "../components/toolBar"
     import modelBkd from "../async/model"
     import relationBkd from "../async/relation"
@@ -23,95 +22,25 @@
             }
         },
         created() {
-            // this.queryModels()
+            this.queryModels()
             // this.queryLinks()
-            this.models = [{
-                name: "User",
-                x: 200,
-                y: 200,
-                fixed: true
-            }, {
-                name: "Company",
-                x: 200,
-                y: 300,
-                fixed: true
-            }]
-            this.links = [{
-                source: 0,
-                target: 1
-            }]
         },
         watch: {
             models() {
-                if (!this.mdlChart) {
-                    this.mdlChart = echarts.init(document.getElementById('pnlModels'))
-                    let option = {
-                        animationDurationUpdate: 1500,
-                        animationEasingUpdate: 'quinticInOut',
-                        series: [
-                            {
-                                type: 'graph',
-                                layout: 'force',
-                                symbolSize: 50,
-                                roam: false,
-                                draggable: false,
-                                animation: false,
-                                label: {
-                                    normal: {
-                                        show: true
-                                    }
-                                },
-                                edgeSymbol: ['circle', 'arrow'],
-                                edgeSymbolSize: [4, 10],
-                                edgeLabel: {
-                                    normal: {
-                                        textStyle: {
-                                            fontSize: 20
-                                        }
-                                    }
-                                },
-                            //     data: this.models,
-                            //     links: this.links
-                            }
-                        ]
-                    }
-                    this.mdlChart.setOption(option)
-                }
-                let option = this.mdlChart.getOption()
-                this.mdlChart.setOption({
-                    graphic: echarts.util.map(this.models, (item, index) => {
-                        return {
-                            id: index,
-                            type: "circle",
-                            position: this.mdlChart.convertToPixel({'seriesIndex': 0}, [item.x, item.y]),
-                            shape: {
-                                cx: 0,
-                                cy: 0,
-                                r: 50/2
-                            },
-                            style: {
-                                fill: "#c33531"
-                            },
-                            draggable: true,
-                            ondrag: echarts.util.curry((dIdx, eve) => {
-                                let pos = this.mdlChart.convertFromPixel({'seriesIndex': 0}, eve.target.position)
-                                this.models[dIdx].x = pos[0]
-                                this.models[dIdx].y = pos[1]
-                                this.mdlChart.setOption({
-                                    graphic: echarts.util.map(this.models, item => {
-                                        position: this.mdlChart.convertToPixel({'seriesIndex': 0}, [item.x, item.y])
-                                    })
-                                })
-                            }, index),
-                            z: 100
-                        }, {
-                            type: "text",
-                            style: {
-                                "text": item.name
-                            }
-                        }
-                    })
-                })
+                d3.select("#pnlModels")
+                    .selectAll("circle")
+                    .data(this.models)
+                    .enter()
+                    .append("circle")
+                    .attr("cx", model => model.x)
+                    .attr("cy", model => model.y)
+                    .attr("r", 20)
+                    .attr("fill", "steelblue")
+                    .call(d3.drag().on("drag", function (tgt) {
+                        d3.select(this)
+                            .attr("cx", tgt.cx = d3.event.x)
+                            .attr("cy", tgt.cx = d3.event.y)
+                    }))
             }
         },
         methods: {
