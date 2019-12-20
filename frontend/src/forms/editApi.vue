26f1b88b3f30246f1eb03bd13005743e6607cfcb
赵佳辰 <opteacher@hotmail.com>
2019-12-20 17:39:11 +0800

    添加接口逻辑修正中

--- frontend/src/forms/editApi.vue
+++ frontend/src/forms/editApi.vue
@@ -1,85 +1,95 @@
 <template>
 <el-form ref="form" :model="api" label-width="80px">
-    <el-form-item label="接口名称" :rules="[
-        { required: true, message: '请输入接口名称', trigger: 'blur' },
-        { min: 3, max: 20, message: '长度在 3 到 20 个字符', trigger: 'blur' }
-    ]" prop="name">
-        <el-input v-model="api.name"/>
-    </el-form-item>
-    <el-form ref="add-param-form" :model="newParam" label-width="80px">
-        <el-form-item label="新增参数" :rules="[
-            { required: true, message: '请输入参数名称', trigger: 'blur' },
-            { min: 3, max: 20, message: '长度在 3 到 20 个字符', trigger: 'blur' }
-        ]" prop="paramName">
-            <el-input v-model="newParam.paramName"/>
-        </el-form-item>
-        <el-form-item label="参数类型" :rules="[
-            { required: true, message: '请输入参数类型', trigger: 'blur' }
-        ]" prop="paramType">
-            <el-col :span="18">
-                <el-select class="w-100" v-model="newParam.paramType" placeholder="请选择">
-                    <el-option v-for="typ in types" :key="typ.name" :label="typ.name" :value="typ.name"/>
+    <el-tabs v-model="activeTab">
+        <el-tab-pane label="基本信息" name="base">
+            <el-form-item label="接口名称" :rules="[
+                { required: true, message: '请输入接口名称', trigger: 'blur' },
+                { min: 3, max: 20, message: '长度在 3 到 20 个字符', trigger: 'blur' }
+            ]" prop="name">
+                <el-input v-model="api.name"/>
+            </el-form-item>
+            <el-form ref="add-param-form" :model="newParam" label-width="80px">
+                <el-form-item label="新增参数" :rules="[
+                    { required: true, message: '请输入参数名称', trigger: 'blur' },
+                    { min: 3, max: 20, message: '长度在 3 到 20 个字符', trigger: 'blur' }
+                ]" prop="paramName">
+                    <el-input v-model="newParam.paramName"/>
+                </el-form-item>
+                <el-form-item label="参数类型" :rules="[
+                    { required: true, message: '请输入参数类型', trigger: 'blur' }
+                ]" prop="paramType">
+                    <el-col :span="18">
+                        <el-select class="w-100" v-model="newParam.paramType" placeholder="请选择">
+                            <el-option v-for="typ in types" :key="typ.name" :label="typ.name" :value="typ.name"/>
+                        </el-select>
+                    </el-col>
+                    <el-col :span="6">
+                        <el-button class="float-right" @click="hdlAddParam">添加参数</el-button>
+                    </el-col>
+                </el-form-item>
+            </el-form>
+            <el-form-item label="参数表" v-if="api.params.length !== 0">
+                <el-table :data="api.params" :show-header="false">
+                    <el-table-column prop="type" width="100">
+                        <template slot-scope="scope">
+                            <el-tag size="medium">{{scope.row.type}}</el-tag>
+                        </template>
+                    </el-table-column>
+                    <el-table-column prop="name"/>
+                    <el-table-column width="80">
+                        <template slot-scope="scope">
+                            <el-button circle type="danger" size="mini" @click.native.prevent="removeParam(scope.row.name)" icon="el-icon-minus"/>
+                        </template>
+                    </el-table-column>
+                </el-table>
+            </el-form-item>
+            <el-form :model="newReturn" ref="add-return-form" label-width="80px">
+                <el-form-item label="返回类型" :rules="[
+                    { required: true, message: '请选择返回值类型', trigger: 'blur' }
+                ]" prop="type">
+                    <el-col :span="18">
+                        <el-select class="w-100" v-model="newReturn.type" placeholder="请选择">
+                            <el-option v-for="typ in types" :key="typ.name" :label="typ.name" :value="typ.name"/>
+                        </el-select>
+                    </el-col>
+                    <el-col :span="6">
+                        <el-button class="float-right" @click="hdlAddReturn">添加返回值</el-button>
+                    </el-col>
+                </el-form-item>
+            </el-form>
+            <el-form-item label="返回值" v-if="api.returns.length !== 0">
+                <el-tag class="mr-2" v-for="ret in api.returns" :key="ret" closable :disable-transitions="false" @close="hdlRemoveRet(ret)">
+                    {{ret}}
+                </el-tag>
+            </el-form-item>
+        </el-tab-pane>
+        <el-tab-pane label="激活方式" name="active">
+                <el-form-item label="激活方式">
+                    <el-select class="w-100" v-model="value" placeholder="请选择">
+                        <el-option v-for="actTyp in activeTypes" :key="actTyp.value" :label="actTyp.label" :value="actTyp.value"/>
+                    </el-select>
+                </el-form-item>
+                <el-form-item v-show="api.enableHttp" label="路由" :rules="[
+                    { required: true, message: '请输入路由', trigger: 'blur' }
+                ]" prop="route">
+                    <el-input v-model="api.route"/>
+                </el-form-item>
+                <el-form-item v-show="api.enableHttp" label="方法" :rules="[
+                    { required: true, message: '请选择方法', trigger: 'blur' }
+                ]" prop="route">
+                    <el-select class="w-100" v-model="api.method" placeholder="请选择">
+                        <el-option v-for="method in methodMap" :key="method" :label="method" :value="method"/>
+                    </el-select>
+                </el-form-item>
+        </el-tab-pane>
+        <el-tab-pane label="其他" name="others">
+            <el-form-item label="绑定模块">
+                <el-select class="w-100" v-model="api.model" placeholder="可选">
+                    <el-option v-for="model in models" :key="model.name" :label="model.name" :value="model.name"/>
                 </el-select>
-            </el-col>
-            <el-col :span="6">
-                <el-button class="float-right" @click="hdlAddParam">添加参数</el-button>
-            </el-col>
-        </el-form-item>
-    </el-form>
-    <el-form-item label="参数表" v-if="api.params.length !== 0">
-        <el-table :data="api.params" :show-header="false">
-            <el-table-column prop="type" width="100">
-                <template slot-scope="scope">
-                    <el-tag size="medium">{{scope.row.type}}</el-tag>
-                </template>
-            </el-table-column>
-            <el-table-column prop="name"/>
-            <el-table-column width="80">
-                <template slot-scope="scope">
-                    <el-button circle type="danger" size="mini" @click.native.prevent="removeParam(scope.row.name)" icon="el-icon-minus"/>
-                </template>
-            </el-table-column>
-        </el-table>
-    </el-form-item>
-    <el-form :model="newReturn" ref="add-return-form" label-width="80px">
-        <el-form-item label="返回类型" :rules="[
-            { required: true, message: '请选择返回值类型', trigger: 'blur' }
-        ]" prop="type">
-            <el-col :span="18">
-                <el-select class="w-100" v-model="newReturn.type" placeholder="请选择">
-                    <el-option v-for="typ in types" :key="typ.name" :label="typ.name" :value="typ.name"/>
-                </el-select>
-            </el-col>
-            <el-col :span="6">
-                <el-button class="float-right" @click="hdlAddReturn">添加返回值</el-button>
-            </el-col>
-        </el-form-item>
-    </el-form>
-    <el-form-item label="返回值" v-if="api.returns.length !== 0">
-        <el-tag class="mr-2" v-for="ret in api.returns" :key="ret" closable :disable-transitions="false" @close="hdlRemoveRet(ret)">
-            {{ret}}
-        </el-tag>
-    </el-form-item>
-    <el-form-item label="HTTP">
-        <el-switch v-model="api.enableHttp"/>
-    </el-form-item>
-    <el-form-item v-show="api.enableHttp" label="路由" :rules="[
-        { required: true, message: '请输入路由', trigger: 'blur' }
-    ]" prop="route">
-        <el-input v-model="api.route"/>
-    </el-form-item>
-    <el-form-item v-show="api.enableHttp" label="方法" :rules="[
-        { required: true, message: '请选择方法', trigger: 'blur' }
-    ]" prop="route">
-        <el-select class="w-100" v-model="api.method" placeholder="请选择">
-            <el-option v-for="method in methodMap" :key="method" :label="method" :value="method"/>
-        </el-select>
-    </el-form-item>
-    <el-form-item label="绑定模块">
-        <el-select class="w-100" v-model="api.model" placeholder="可选">
-            <el-option v-for="model in models" :key="model.name" :label="model.name" :value="model.name"/>
-        </el-select>
-    </el-form-item>
+            </el-form-item>
+        </el-tab-pane>
+    </el-tabs>
 </el-form>
 </template>
 
@@ -89,6 +99,7 @@ import backend from "../backend"
 export default {
     data() {
         return {
+            activeTab: "base",
             methodMap: [
                 "GET", "POST", "PUT", "DELETE", "PATCH"
             ],
@@ -109,7 +120,17 @@ export default {
                 type: ""
             },
             models: [],
-            types: []
+            types: [],
+            activeTypes: [{
+                label: "HTTP接口",
+                value: "http"
+            }, {
+                label: "RPC接口",
+                value: "rpc"
+            }, {
+                label: "定时任务",
+                value: "timing"
+            }]
         }
     },
     async created() {
